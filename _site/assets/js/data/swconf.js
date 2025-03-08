87bf82ad1b3111fa9fd7326901a01122e757bc2f
Hyubbbb <ekwjsxl200@gmail.com>
2025-03-09 00:11:48 +0900

    Fix: add x86_64-linux platform to Gemfile.lock

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741446263',resources: [
+    cacheName: 'chirpy-1741446690',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
