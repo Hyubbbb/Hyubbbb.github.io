c57ebfe69a4626017935ca4c98dbaffd84dafa78
Hyubbbb <ekwjsxl200@gmail.com>
2025-03-09 00:25:33 +0900

    변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741446690',resources: [
+    cacheName: 'chirpy-1741447519',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
