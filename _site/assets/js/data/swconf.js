cfc7c2442bd9f0623b72a54383149d32be544feb
Hyubbbb <ekwjsxl200@gmail.com>
2025-03-09 23:32:09 +0900

    upload linkedin link

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741448000',resources: [
+    cacheName: 'chirpy-1741530700',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
