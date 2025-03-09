2c3e71d581163ba4a52fa650bf6ca05294833344
Hyubbbb <ekwjsxl200@gmail.com>
2025-03-09 23:35:40 +0900

    error fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741530700',resources: [
+    cacheName: 'chirpy-1741530743',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
